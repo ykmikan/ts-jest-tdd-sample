e962c917a4750940abcbef9cffce91733799f64d
takasp <takasp.2350@gmail.com>
2019-06-02 21:21:40 +0900

    Add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {},
+};
